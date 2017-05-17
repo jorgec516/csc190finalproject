77ccbd0c3c19ee239da48c2ba36b50ff81d536ee
Stefan Bozic <sbozic1@pride.hofstra.edu>
2017-05-17 19:08:25 -0400

    Stefan

---
+++ Stefan/getDate.java
@@ -0,0 +1,14 @@
+public class getDates{
+	//year-month-date;
+	//yyyy-mm-dd
+	int startDate;
+	int endDate;
+
+	
+	public static void givenDates(String val){
+	String qry = "SELECT dates FROM stockHistory where dates=" + val;
+	Utils.execNonQuery(qry);
+	}
+
+	
+}
